92ec866892a00c3de16fe32b55504fb5395b0965
iSalah-YT <82966610+iSalah-YT@users.noreply.github.com>
2026-04-01 17:55:13 +0100

    Authentication done

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
+import usersRoutes from '#routes/users.routes.js';
 
 const app = express();
 
@@ -19,9 +22,31 @@ app.use(
   })
 );
 
+app.use(securityMiddleware);
+
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
+
   res.status(200).send('Hello from Acquisitions!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
 });
 
 export default app;
